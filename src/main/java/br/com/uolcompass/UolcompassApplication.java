16e4668b3e321781ea7e836edc2d2ef4952d2e02
erick-ferraz <erickfpc.tech@gmail.com>
2026-06-22 19:37:41 -0300

    Initial commit

---
+++ src/main/java/br/com/uolcompass/UolcompassApplication.java
@@ -0,0 +1,13 @@
+package br.com.uolcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolcompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolcompassApplication.class, args);
+	}
+
+}
